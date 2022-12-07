9c963634940819bf11852d33fd2ce512979799d4
PaperNick <papernick@users.noreply.github.com>
2022-12-07 10:10:48 +0200

    Break the rest_reminder.sh script into multiple lines

--- rest_reminder.sh
+++ rest_reminder.sh
@@ -1,1 +1,7 @@
-echo "notify-send -i gnome-set-time 'Break Time!' 'Stand up, move around and refresh your eyes for a couple of minutes.' && sleep 10m && eval '$(readlink -f "$0")'" | at now + 60 minutes
+#!/bin/bash
+
+SCRIPT_FULL_PATH="$(readlink -f "$0")"
+
+notify-send -t 10000 -i gnome-set-time 'Break Time!' 'Stand up, move around and refresh your eyes for a couple of minutes.'
+
+echo "sleep 10m && '$SCRIPT_FULL_PATH'" | at now + 60 minutes
