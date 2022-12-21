47774e36f4625e4bc43bf02def43e340e8c0d863
PaperNick <papernick@users.noreply.github.com>
2022-12-21 16:40:09 +0200

    Change rest_reminder.sh script to schedule reminders without sleep

--- rest_reminder.sh
+++ rest_reminder.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SCRIPT_FULL_PATH="$(readlink -f "$0")"
+REMIND_AFTER_MINUTES=70
 
 notify-send -t 10000 -i gnome-set-time 'Break Time!' 'Stand up, move around and refresh your eyes for a couple of minutes.'
 
-echo "sleep 10m && '$SCRIPT_FULL_PATH'" | at now + 60 minutes
+echo "'$SCRIPT_FULL_PATH'" | at now + $REMIND_AFTER_MINUTES minutes
