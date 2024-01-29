94b65945843babb29d222cfa57d508d5c657bc43
PaperNick <papernick@users.noreply.github.com>
2024-01-29 23:39:28 +0200

    Night theme script: change panel theme along with everything else

--- toggle_night_theme.sh
+++ toggle_night_theme.sh
@@ -5,7 +5,9 @@ CURRENT_THEME="$(xfconf-query -c xfwm4 -p /general/theme)"
 if [ "$CURRENT_THEME" = "Greybird" ]; then
     xfconf-query -c xfwm4 -p /general/theme -s Greybird-dark
     xfconf-query -c xsettings -p /Net/ThemeName -s Greybird-dark
+    xfconf-query -c xfce4-panel -p /panels/dark-mode -s true
 else
     xfconf-query -c xfwm4 -p /general/theme -s Greybird
     xfconf-query -c xsettings -p /Net/ThemeName -s Greybird
+    xfconf-query -c xfce4-panel -p /panels/dark-mode -s false
 fi
