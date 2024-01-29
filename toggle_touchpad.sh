b72bbad181b49316f8fdd0cee0ae8c9aa1137455
PaperNick <papernick@users.noreply.github.com>
2024-01-29 23:40:53 +0200

    Toggle touchpad script: add status notification

--- toggle_touchpad.sh
+++ toggle_touchpad.sh
@@ -3,3 +3,9 @@
 current_touchpad_value=$(synclient -l | grep TouchpadOff | awk '{print $3}')
 toggled_touchpad_value=$((1 - $current_touchpad_value))
 synclient TouchpadOff=$toggled_touchpad_value
+
+if [ "$toggled_touchpad_value" = "0" ]; then
+    notify-send -t 3000 -i input-touchpad-symbolic 'Touchpad On' 'Touchpad is now enabled'
+else
+    notify-send -t 3000 -i touchpad-disabled-symbolic 'Touchpad Off' 'Touchpad has been disabled'
+fi
