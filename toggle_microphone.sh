de61297954ab4c1d8f6ff835ebf42f36465eb5d5
PaperNick <papernick@users.noreply.github.com>
2024-05-20 22:25:38 +0300

    Toggle mic script: use pacmd to toggle mic, which doesn't rely on init order

--- toggle_microphone.sh
+++ toggle_microphone.sh
@@ -4,13 +4,17 @@
 # Inspired from: https://askubuntu.com/a/1152002
 
 toggle_builtin_mic() {
-    builtin_mic_toggle_output="$(amixer set Capture toggle)"
-    if [ "$(echo $builtin_mic_toggle_output | grep '\[on\]')" != '' ]; then
-        # Mic is on - return value 1
-        echo 1
-    else
+    pacmd list-sources | grep -B 60 'analog-input-internal-mic:' | grep -oP 'index: \d+' | awk '{print $2}' | xargs -I {} pactl set-source-mute {} toggle
+}
+
+builtin_mic_status() {
+    local is_builtin_mic_muted="$(pacmd list-sources | grep -B 60 'analog-input-internal-mic:' | grep muted | awk '{print $2}')"
+    if [ "$(echo $is_builtin_mic_muted)" = 'yes' ]; then
         # Mic is off - return value 0
         echo 0
+    else
+        # Mic is on - return value 1
+        echo 1
     fi
 }
 
@@ -23,8 +27,8 @@ unmute_usb_mic() {
 }
 
 main() {
-    is_builtin_mic_on="$(toggle_builtin_mic)"
-
+    toggle_builtin_mic
+    is_builtin_mic_on="$(builtin_mic_status)"
     if [ "$is_builtin_mic_on" = "1" ]; then
         unmute_usb_mic
         notify-send -t 3000 -i audio-input-microphone-symbolic 'Microphone On' 'You microphone is now enabled'
