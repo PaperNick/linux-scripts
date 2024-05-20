322576c83b7565ee35df87a25ba190200c9b7138
PaperNick <papernick@users.noreply.github.com>
2024-05-20 15:03:12 +0300

    Toggle mic script: mute built-in and external USB mic at the same time

--- toggle_microphone.sh
+++ toggle_microphone.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-mic_toggle_output="$(amixer set Capture toggle)"
-if [ "$(echo $mic_toggle_output | grep '\[on\]')" != '' ]; then
-    is_mic_on=1
-else
-    is_mic_on=0
-fi
+# Mute/Unmute built-in and external USB mics
+# Inspired from: https://askubuntu.com/a/1152002
 
-if [ "$is_mic_on" = "1" ]; then
-    notify-send -t 3000 -i audio-input-microphone-symbolic 'Microphone On' 'You microphone is now enabled'
-else
-    notify-send -t 3000 -i microphone-sensitivity-muted-symbolic 'Microphone Muted' 'You microphone has been muted'
-fi
+toggle_builtin_mic() {
+    builtin_mic_toggle_output="$(amixer set Capture toggle)"
+    if [ "$(echo $builtin_mic_toggle_output | grep '\[on\]')" != '' ]; then
+        # Mic is on - return value 1
+        echo 1
+    else
+        # Mic is off - return value 0
+        echo 0
+    fi
+}
+
+mute_usb_mic() {
+    pacmd list-sources | grep -i -C 1 Blue_Microphones | grep -oP 'index: \d+' | awk '{print $2}' | xargs -I {} pactl set-source-mute {} 1
+}
+
+unmute_usb_mic() {
+    pacmd list-sources | grep -i -C 1 Blue_Microphones | grep -oP 'index: \d+' | awk '{print $2}' | xargs -I {} pactl set-source-mute {} 0
+}
+
+main() {
+    is_builtin_mic_on="$(toggle_builtin_mic)"
+
+    if [ "$is_builtin_mic_on" = "1" ]; then
+        unmute_usb_mic
+        notify-send -t 3000 -i audio-input-microphone-symbolic 'Microphone On' 'You microphone is now enabled'
+    else
+        mute_usb_mic
+        notify-send -t 3000 -i microphone-sensitivity-muted-symbolic 'Microphone Muted' 'You microphone has been muted'
+    fi
+}
+
+main
